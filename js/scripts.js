bc2f6ed1542d3997d3661a78e0c4142e5bfc494b
rafael.lumang <lumang.rafael@gmail.com>
2022-09-20 18:34:00 -0700

    Added exit modal function

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,12 @@
 const gallery = document.querySelector('#gallery');
 const body = document.body;
 
-// const modal = `
-  
-        
-
-//     `
-//     body.insertAdjacentHTML('beforeend', modal);
-
-//     const modalContainer = document.querySelector('.modal-container');
-//     modalContainer.style.display = 'none';
-
-    
-//     const closeModal = document.querySelector('#modal-close-btn');
-//     closeModal.addEventListener ('click', () => {
-//         modalContainer.style.display = 'none';
-//     })
-
 fetch('https://randomuser.me/api/?nat=us&?inc=picture,name,email,location,cell,dob&results=12')
     .then(res => res.json())
     .then(data => 
         {const profiles = data.results
     generateCard (profiles);
-    console.log(profiles);
+   
         })
 
 
@@ -50,17 +34,18 @@ function generateCard(data) {
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => card.addEventListener('click', (event) => {
         const index = event.currentTarget.dataset.indexNumber;
-        insertModal(data, index);
+        generateModal(data, index);
         console.log(index);
-        modalContainer.style.display = 'block';
+        // modalContainer.style.display = 'block';
        
         }) 
     )
 };
 
 
+/*MODAL MARKUP*/
 
-function insertModal(data, index) {
+function generateModal(data, index) {
     const modalHTML = `
     <div class="modal-container">
     <div class="modal">
@@ -73,11 +58,23 @@ function insertModal(data, index) {
     <hr>
     <p class="modal-text">${data[index].phone}</p>
     <p class="modal-text">${data[index].location.street.number} ${data[index].location.street.name}, ${data[index].location.city}, ${data[index].location.state} ${data[index].location.postcode}</p>
-    <p class="modal-text">Birthday: ${data[index].dob}</p>
+    <p class="modal-text">Birthday: ${data[index].dob.date}</p>
     </div>
     </div>
     `
 
-    // const modalInfo = document.querySelector('modal-info-container');
     body.insertAdjacentHTML('beforeend', modalHTML);
+
+
+    const modalExit = document.querySelector('.modal-close-btn');
+    modalExit.addEventListener('click', () => {
+       closeModal();
+
+    })
+}
+
+function closeModal() {
+    const modalContainer = document.querySelector('.modal-container');
+    modalContainer.remove();
+    modal.style.display = 'none';
 }
